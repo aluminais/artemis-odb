e40ccf45d33950794801f0e4401b677233cd8e51
aluminais <austinluminais@gmail.com>
2023-10-28 21:20:26 -0500

    Remove tests

--- artemis-core/artemis-test/src/main/java/com/artemis/component/PooledString.java
+++
@@ -1,9 +0,0 @@
-package com.artemis.component;
-
-import com.artemis.Component;
-import com.artemis.annotations.PooledWeaver;
-
-@PooledWeaver
-public class PooledString extends Component {
-	public String s;
-}
--- artemis-core/artemis-test/src/main/java/com/artemis/component/link/MultiLinkSkipTargetCheck.java
+++
@@ -1,9 +0,0 @@
-package com.artemis.component.link;
-
-import com.artemis.Component;
-import com.artemis.Entity;
-import com.artemis.utils.Bag;
-
-public class MultiLinkSkipTargetCheck extends Component {
-	public Bag<Entity> other = new Bag<Entity>();
-}
